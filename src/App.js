d8504a271816224da25590ecd86a81c48fa754a4
paulaandrezza <paulaandrezza25@gmail.com>
2023-03-21 10:41:46 -0300

    Finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./global.css";
+import "./styles.css";
+import { IoMdAdd } from "react-icons/io";
+import { MdOutlineClose } from "react-icons/md";
+import { useState } from "react";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  const [task, setTask] = useState("")
+
+  const [tasks, setTasks] = useState([])
+
+  function handleCreateTask() {
+    if (task === "") {
+      // Error message
+      toast.error("Digite alguma task")
+    } else {
+      // Add task
+      const idRandom = (num) => Math.floor(Math.random() * num)
+      const newTask = { id: idRandom(1000000), title: task, isComplete: false }
+
+      setTasks([ ...tasks, newTask ])
+
+      setTask("")
+
+      // TODO: Utilizar o localStorage para armazenar as tasks do usuário
+
+    }
+  }
+
+  function handleToggleTaskCompletion(id) {
+    const taskComplete = tasks.map(task => {
+      if(task.id === id) {
+        return{ ...task, isComplete: !task.isComplete }
+      }
+
+      return task
+    })
+
+    setTasks(taskComplete)
+  }
+
+  function handleDeleteTask(id) {
+    setTasks(tasks.filter(remove => remove.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <ToastContainer />
+
+      <div className="todo">
+
+        <header>
+
+          <input type="text" value={task} onChange={(ev) => setTask(ev.target.value)} />
+
+          <button onClick={handleCreateTask}><IoMdAdd /></button>
+
+        </header>
+
+        {tasks.map(task => (
+
+          <div key={task.id} className={task.isComplete ? "task-container completed" : "task-container"}>
+          <div className="check-and-title">
+            <label className="checkbox-container">
+              <input type="checkbox" onClick={() => handleToggleTaskCompletion(task.id)} />
+              <span className="checkmark"></span>
+            </label>
+            <p>{task.title}</p>
+          </div>
+
+          <div><MdOutlineClose onClick={() => handleDeleteTask(task.id)} /></div>
+
+          </div>
+
+        ))}
+
+      </div>
     </div>
   );
 }
